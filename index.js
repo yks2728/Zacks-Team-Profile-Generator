6261f560ff388027989744a3740508df7c023e30
Zack Greenfield <zackg007@verizon.net>
2022-01-30 15:51:59 -0500

    created folders plus Employee class

---
+++ index.js
@@ -0,0 +1,7 @@
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
