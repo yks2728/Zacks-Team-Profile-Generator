219d2ed6e3510b51b35f25151e898f7f00899bdd
Zack Greenfield <zackg007@verizon.net>
2022-02-04 10:32:43 -0500

    built inquirer prompts for manager

--- index.js
+++ index.js
@@ -5,3 +5,61 @@ const Inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 
+const promptUser = () => {
+    return Inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the Managers Name (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Managers name!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the managers employee id (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the managers employee id!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the managers email (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the managers email!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'Enter the managers office number (Required)',
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the managers office number!')
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'addTeamMember',
+            message: 'Would you like to add another team member? (Required)',
+            choices: ['Yes', 'No']
+        }
+    ])
+}
