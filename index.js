9b1590c0e960b259b4edc4327d86941bb1978be8
Zack Greenfield <zackg007@verizon.net>
2022-02-05 13:03:24 -0500

    added prompts for engineer and intern

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const inquire = require('inquirer');
 const path = require('path');
 const generatePage = require('./src/page-template.js');
 const { writeFile } = require('./utils/generate-site.js');
+const teamMembers = [];
 
 
-const questions = () => {
+const addManager  = () => {
     return inquire.prompt([
         {
             type: 'input',
@@ -61,20 +62,149 @@ const questions = () => {
             type: 'confirm',
             name: 'addTeamMember',
             message: 'Would you like to add this team member? (Required)',
-            default: true
+            default: false
+        }
+    ]);
+};
+
+addManager()
+    .then(data => {
+        console.log(data)
+    })
+
+const addEngineer = () => {
+    return inquire.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the Engineers name (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Engineers name!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter the Engineers Id (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Engineers Id!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Engineers Email (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Engineers Email!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter the Engineers Github Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Engineers Github Username!');
+                }
+            }
         },
         {
             type: 'confirm',
-            name: 'addAnotherTeamMember',
-            message: 'Would you like to add another team member (Required)',
-            default: true
+            name: 'addTeamMember',
+            message: 'Would you like to add this team member?',
+            default: false
+        }
+    ]);
+};
+
+addEngineer()
+    .then(data => {
+        console.log(data)
+    })
+
+
+const addIntern = () => {
+    return inquire.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter the Interns Name (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Interns name!');
+                }
+            }
         },
         {
-            type: 'list',
-            name: 'Employee',
-            message: 'Select another employee to add to the team (Required)',
-            choices: ['Engineer', 'Intern']
+            type: 'input',
+            name: 'id',
+            message: 'Enter the Interns Id (Required)',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the Interns id!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter the Interns email (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Interns email!');
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter the Interns School (Required)',
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log('Please Enter the Interns School!');
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'addTeamMember',
+            message: 'Would you like to add this team member?',
+            default: false
         }
-    ])
+    ]);
+};
 
-}
+
+addIntern()
+    .then(data => {
+        console.log(data)
+    })
+
+
+
+
+
+
+
