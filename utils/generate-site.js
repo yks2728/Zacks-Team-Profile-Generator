a5d5b061eba8286e8f0bfa33f373fe4cc209245f
Zack Greenfield <zackg007@verizon.net>
2022-02-06 12:36:38 -0500

    updated file path name

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -18,7 +18,7 @@ const writeFile = fileContent => {
 
 const copyFile = () => {
     return new Promise((resolve, reject) => {
-        fs.copyFile('./src/style.css', './dis/style.css', err => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
             if (err) {
                 reject(err);
                 return;
